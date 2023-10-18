8021b538366204dd8612024aaf601281061b0227
hajara munjiya <hajaramunjiya23@gmail.com>
2023-10-18 23:42:28 +0400

    chapter 1 exercises

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise 1.1.py
@@ -0,0 +1,14 @@
+# This program prints the Twinkle, Twinkle, Little Star poem in a specific format.
+
+# First verse
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder what you are!")  # Indented with a tab
+
+# Second verse
+print("\t\tUp above the world so high,")  # Indented with two tabs
+print("\t\tLike a diamond in the sky.")
+
+# Third verse
+print("Twinkle, twinkle, little star,")  # Back to the first-level indentation
+print("\tHow I wonder what you are")  # No exclamation mark to match the original poem
+
