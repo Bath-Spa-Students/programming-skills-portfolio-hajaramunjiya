387a47c5aab56d657e04db09e289a39d67556c66
hajara munjiya <hajaramunjiya23@gmail.com>
2023-10-18 23:42:51 +0400

    chapter 1 practice

---
+++ Chapter 1- Getting Started with Python/Exercises/practice 2.py
@@ -0,0 +1,8 @@
+#Create a program to output "hi" 50 times, without any separator
+
+def print_hi_fifty_times():
+    for _ in range(50):
+        print("hi", end="")
+
+# Call the function to print "hi" 50 times
+print_hi_fifty_times()
