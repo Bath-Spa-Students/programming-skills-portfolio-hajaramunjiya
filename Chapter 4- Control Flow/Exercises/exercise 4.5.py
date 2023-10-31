69e4f9b94f4ad9ca979c763fbd412eaa532fd3b0
hajaramunjiya <146180728+hajaramunjiya@users.noreply.github.com>
2023-10-31 10:36:13 +0400

    Update exercise 4.5.py

--- Chapter 4- Control Flow/Exercises/exercise 4.5.py
+++ Chapter 4- Control Flow/Exercises/exercise 4.5.py
@@ -1,23 +1,24 @@
-# Make a list of your three favorite fruits and call it favorite_fruits.
-favorite_fruits = ['apples', 'bananas', 'strawberries']
+# Define a list of favorite fruits.
+favorite_fruits = ["apple", "banana", "strawberry"]
 
-# Check if certain fruits are in your list and print a statement accordingly.
-if 'bananas' in favorite_fruits:
-    # If 'bananas' are in your list, print a statement.
-    print("You really like bananas!")
+# Check if each fruit is in the list of favorite fruits and print a corresponding message.
 
-if 'apples' in favorite_fruits:
-    # If 'apples' are in your list, print a statement.
+# Check if "apple" is in the list.
+if "apple" in favorite_fruits:
     print("You really like apples!")
 
-if 'oranges' in favorite_fruits:
-    # If 'oranges' are in your list, print a statement.
-    print("You really like oranges!")
+# Check if "banana" is in the list.
+if "banana" in favorite_fruits:
+    print("You really like bananas!")
 
-if 'strawberries' in favorite_fruits:
-    # If 'strawberries' are in your list, print a statement.
+# Check if "kiwi" is in the list.
+if "kiwi" in favorite_fruits:
+    print("You really like kiwis!")
+
+# Check if "strawberry" is in the list.
+if "strawberry" in favorite_fruits:
     print("You really like strawberries!")
 
-if 'kiwis' in favorite_fruits:
-    # If 'kiwis' are in your list, print a statement.
-    print("You really like kiwis!")
+# Check if "orange" is in the list.
+if "orange" in favorite_fruits:
+    print("You really like oranges!")
