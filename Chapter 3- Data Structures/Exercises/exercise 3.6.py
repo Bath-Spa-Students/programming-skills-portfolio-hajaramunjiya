fa6397b8691f92ab0b1ca7c40eb00fab3b5388bd
hajaramunjiya <146180728+hajaramunjiya@users.noreply.github.com>
2023-11-15 10:38:30 +0400

    exercise 3.6.py

---
+++ Chapter 3- Data Structures/Exercises/exercise 3.6.py
@@ -0,0 +1,20 @@
+# Initial list of guests
+guests = ['Alice', 'Bob', 'Charlie', 'David']
+
+# Print a message about the limit of guests
+print("Due to a delay, the new dinner table won't arrive in time. You can only invite two people for dinner.")
+
+# Use pop() to remove guests until only two names remain
+while len(guests) > 2:
+    removed_guest = guests.pop()
+    print(f"Sorry, {removed_guest}, I can't invite you to dinner.")
+
+# Print a message to the two remaining guests
+for remaining_guest in guests:
+    print(f"{remaining_guest}, you're still invited to dinner.")
+
+# Use del to remove the last two names from the list
+del guests[:]
+
+# Print the empty list to confirm
+print("The final guest list is:", guests)
