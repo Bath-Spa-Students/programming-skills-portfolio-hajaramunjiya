e632133ad8bfdedcacf8f5131b962c94097a35e9
hajaramunjiya <146180728+hajaramunjiya@users.noreply.github.com>
2023-11-17 10:52:30 +0400

    exercise 7.1.py

--- Chapter 7- Functions/Exercises/exercise 7.1.py
+++ Chapter 7- Functions/Exercises/exercise 7.1.py
@@ -1,6 +1,6 @@
 def display_message():
     """Prints a message about what is being learned in this chapter."""
-    print("In this chapter, we are learning about functions in Python.")
+    print("In this chapter, I'm learning about functions in Python.")
 
 # Call the function
 display_message()
